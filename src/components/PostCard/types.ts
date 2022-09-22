dfc5251538b3fb2daf8d1c937372dfbd63696678
André Canuto <andrecanuto@MacBook-Pro-de-Andre.local>
2022-09-21 23:15:42 -0300

    feat:routes and typescript config done

---
+++ src/components/PostCard/types.ts
@@ -0,0 +1,5 @@
+export interface PostCardPropTypes {
+    id: number
+    title: string
+    body: string   
+}
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import PostCard from '../../components/PostCard'
+
+function PostsPage() {
+  return (
+    <div>
+      <div>
+        <PostCard id={1} title='teste' body='teste' />
+      </div>
+    </div>
+  );
+}
+
+export default PostsPage;
